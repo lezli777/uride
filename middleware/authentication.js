6b8e41e6798d92dbb15dbb8d5cc5b8e1316e2515
lezli777 <119299316+lezli777@users.noreply.github.com>
2022-12-21 12:50:27 +0530

    response changes auth

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,17 +1,20 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
+const {errorResponse} = require('../helpers/apiResponse.js')
 
 const verifyToken = (req, res, next) => {
   const token =
     req.body.token || req.query.token || req.headers["x-access-token"];
   if (!token) {
-    return res.status(403).send("A token is required for authentication");
+    //return res.status(403).send("A token is required for authentication");
+    return errorResponse(res, 'A token is required for authentication')
   }
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     req.user = decoded;
   } catch (err) {
-    return res.status(401).send("Invalid  or Expired");
+    //return res.status(401).send("Invalid  or Expired");
+    return errorResponse(res, 'Invalid  or Expired')
   }
   return next();
 
